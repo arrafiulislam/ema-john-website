956cf8a346ba6e4fdc5efe9bc8bac7b69ba1db61
unknown <volumark@gmail.com>
2024-01-25 14:49:26 +0600

    Retrive Done

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -13,11 +14,26 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data));
     },[])
+
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+            setCart(savedCart);
+        }
+    },[products])
    
     const handleAddtoCart = (product) => {
         //  cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
